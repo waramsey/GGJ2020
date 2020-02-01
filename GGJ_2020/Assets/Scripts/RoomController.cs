9133b0b2e88bc9be895cd453616604d68367c80c
waramsey <46686986+waramsey@users.noreply.github.com>
2020-02-01 16:22:53 -0600

    Camera Work

---
+++ GGJ_2020/Assets/Scripts/RoomController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomController : MonoBehaviour //attach to room
+{
+    public GameObject room;
+    public GameObject virtualCamera;
+    public string Objective;
+    public GameObject[] doors; //fill this with door prefabs
+    public Canvas canvas;
+
+
+    public void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Player") && !other.isTrigger) {
+            virtualCamera.SetActive(true);
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D other) {
+        if (other.CompareTag("Player") && !other.isTrigger) {
+            virtualCamera.SetActive(false);
+        }
+    }
+
+    void Start()
+    {
+        //Yeet Player??
+
+        if (room.tag == "incomplete") {
+            for (int i = 0; i < doors.Length; i++) {
+                Spawn(doors[i], new Vector2(doors[i].transform.position.x, doors[i].transform.position.y));
+            }
+
+
+        }
+        //Display Objective if room not complete
+        //Undisplay objective
+
+
+        //Spawn entities on delay if room not complete
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //check if objective complete
+        //if complete, update room tag
+        //destroy doors
+    }
+
+    void Spawn(GameObject g, Vector2 v) {
+        Instantiate(g, v, Quaternion.identity);
+    }
+}
---
+++ GGJ_2020/Assets/Scripts/Timer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public float time;
+    public float startTime;
+    public Text scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 60.0f;
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time -= Time.deltaTime;
+        scoreText.text = time.ToString("0");
+        if (time < 0) {
+            //end game
+        }
+    }
+}
